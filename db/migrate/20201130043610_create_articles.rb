160f1452eb0135bbac1d0bd4e72beb1b28ff3aa1
Tram Lai <hongoctram17@gmail.com>
2020-11-29 21:47:40 -0800

    change database.yml

--- db/migrate/20201130043610_create_articles.rb
+++ db/migrate/20201130043610_create_articles.rb
@@ -2,10 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.0]
   def change
     create_table :articles do |t|
       t.string :title
-      t.string :string
-      t.text :description
 
-      t.timestamps
     end
   end
 end
