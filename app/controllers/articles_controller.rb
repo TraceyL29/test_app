6a6270e31b209561bf980562055d1fa9ad7bf137
Tram Lai <hongoctram17@gmail.com>
2020-12-02 14:54:36 -0800

    show data feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,14 +4,13 @@ class ArticlesController < ApplicationController
   # GET /articles
   # GET /articles.json
   def index
-    @articles = Article.all
+    @article = Article.all
   end
-
   # GET /articles/1
   # GET /articles/1.json
   def show
     @article = Article.find(params[:id])
-    
+
   end
 
   # GET /articles/new
