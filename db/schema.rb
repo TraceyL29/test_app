c476ffbfac59645604176240d2c7669fd6b28dd4
Tram Lai <hongoctram17@gmail.com>
2020-12-02 13:39:38 -0800

    connect datase, add data and retrieve data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_01_223410) do
+ActiveRecord::Schema.define(version: 2020_12_01_234222) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "adminpack"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_12_01_223410) do
     t.string "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.text "description"
   end
 
 end
